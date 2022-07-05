43bbb043548fcfab557987842f38b6916bd812ea
unknown <mcsalazar122@gmail.com>
2022-07-04 22:25:05 -0500

    Employee setup

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,8 +1,19 @@
 const Employee = require('../lib/Employee.js');
 
-test ('creates employee object', () => {
+test ('creates a new employee object', () => {
     const employee = new Employee('Nick');
 
     expect(employee.name).toBe('Nick');
-   // expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.title).toBe('Manager');
+    expect(employee.email).toBe('nick@example.com');
+    expect(employee.id).toEqual(expect.any(Number));
 });
+
+test('gets employee type as an object', () => {
+    const employee = new Employee('Nick');
+    expect(employee.getType()).toBe('type');
+    //expect(employee.getType()).toBe('icon');
+    
+});
+
+
